e773e984f17c2dfe164a566570b226bff4049125
gusflus <gus.flusser@gmail.com>
2025-10-25 12:51:29 -0700

    chore: cdk init

---
+++ apps/next-suite/README.md
@@ -0,0 +1,1 @@
+for the service suite app
---
+++ test/unwrapathon-gus_bus.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UnwrapathonGusBus from '../lib/unwrapathon-gus_bus-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/unwrapathon-gus_bus-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UnwrapathonGusBus.UnwrapathonGusBusStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
